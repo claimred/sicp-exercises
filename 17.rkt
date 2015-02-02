03cb72638f5ea95dec77c1886a7234fdee116fc8
claimred <t_last@mail.ru>
2015-02-02 23:54:19 +0400

    part of 2.6

--- 17.rkt
+++ 17.rkt
@@ -38,7 +38,34 @@
 
   
 
+;;; 2.6
+(define zero (lambda (f) (lambda (x) x)))
 
+(define (add-1 n)
+  (lambda (f) 
+    (lambda (x) 
+      (f ((n f) x)))))
+
+;; (add-1 zero)
+;; (lambda (f) (lambda (x) (f ((zero f) x))))
+;; (lambda (f) (lambda (x) (f (((lambda (f) (lambda (x) x)) f) x)))), (lambda (f) (lambda (x) x)) f = identity
+;; (lambda (f) (lambda (x) (f x)))
+
+(define one
+  (lambda (f) (lambda (x) (f x))))
+
+;; (add-1 one)
+;; (lambda (f) (lambda (x) (f ((one f) x))))
+;; (lambda (f) (lambda (x) (f (f x))))
+
+(define two
+  (lambda (f) (lambda (x) (f (f x)))))
+
+;;(define (plus a b))
+
+
+ (define (church-to-int cn) 
+   ((cn (lambda (n) (+ n 1))) 0)) 
 
 
   
