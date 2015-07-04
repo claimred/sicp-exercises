1e943366e3dcc37074369e8f0882b5cc23bdf0d3
claimred <t_last@mail.ru>
2015-07-05 01:25:34 +0300

    2.20

--- 19.rkt
+++ 19.rkt
@@ -40,5 +40,48 @@
   (list-append (list 1 2 3 4 5) (list-reverse (list 1 2 3 4 5))))
 
 
+;;; 2.19
+
+(define (no-more? l)
+  (null? l))
+
+(define (except-first-denomination l)
+  (cdr l))
+
+(define (first-denomination l)
+  (car l))
+
+;; The order of the coin-values elements doesn't matter
+;; because
+(define us-coins (list 25 50 10 1 5))
+(define uk-coins (list 50 100 20 10 5 2 1 0.5))
+
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else
+         (+ (cc amount
+                (except-first-denomination coin-values))
+            (cc (- amount
+                   (first-denomination coin-values))
+                coin-values)))))
+
+;;; 2.20
+
+(define (same-parity x . z)
+  (define (sp-iter l f)
+    (if (null? l)
+        nil
+        (if (f (car l))
+            (cons (car l) (sp-iter (cdr l) f))
+            (sp-iter (cdr l) f))))
+  (sp-iter z
+           (lambda (y) (if (even? x)
+                           (even? y)
+                           (not (even? y))))))
+  
+(same-parity 2 3 4 5 6 8 10 11 13 16 17 17 19 20)
+(same-parity 1 2 3 4 5 6 8 10 11 13 16 17 17 19 20)
+
 
   
