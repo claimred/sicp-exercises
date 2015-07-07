f3483322e66e65b368a02bbc0aa76ec3911350f4
claimred <t_last@mail.ru>
2015-07-07 12:33:40 +0300

    2.34, 2.35

---
+++ 23.rkt
@@ -0,0 +1,52 @@
+#lang planet neil/sicp
+
+;; enumerator, filter, map, accumulator
+
+(define (map proc items)
+  (if (null? items)
+      nil
+      (cons (proc (car items))
+            (map proc (cdr items)))))
+
+(define (accumulate op init seq)
+  (if (null? seq)
+      init
+      (op (car seq) (accumulate op init (cdr seq)))))
+
+(define (filter predicate seq)
+  (cond ((null? seq) nil)
+        ((predicate (car seq))
+         (cons (car seq)
+               (filter predicate (cdr seq))))
+         (else (filter predicate (cdr seq)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (enumerate-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (list tree))
+        (else (append (enumerate-tree (car tree)) (enumerate-tree (cdr tree))))))
+
+(define (sum-odd-squares tree) 
+  (accumulate + 0 (map (lambda (x) (* x x)) (filter odd? (enumerate-tree tree)))))
+
+;;; 2.33
+
+(define (new-map proc seq)
+  (accumulate (lambda (x y) (cons (proc x) y)) nil seq))
+
+(define (new-append seq1 seq2)
+  (accumulate cons seq2 seq1))
+
+(define (new-length seq)
+  (accumulate (lambda (x y) (+ 1 y)) 0 seq))
+
+;;; 2.34
+
+(define (horner-eval x coefficient-sequence)
+  (accumulate (lambda (this-coeff higher-terms) (+ this-coeff (* x higher-terms)) )
+              0
+              coefficient-sequence))
